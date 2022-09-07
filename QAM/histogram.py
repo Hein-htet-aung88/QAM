7a7109b61625a39e7a829b0a2f012774fab131b0
heinaung <hein8htet8@gmail.com>
2022-09-07 21:54:33 +0800

    updated histogram to remove describe function

--- QAM/histogram.py
+++ QAM/histogram.py
@@ -10,4 +10,4 @@ df['daily_changes'] = df['Adj Close'].pct_change(periods = 1)
 sns.displot(df['daily_changes'].dropna(), bins=100, color='purple', kde=True, height = 6, aspect = 1.5)
 plt.suptitle('Daily % return of BITCOIN (USD) price, 05-09-2021 to 04-09-2022', fontsize=12, color='black')
 plt.grid(True)
-plt.show()
+plt.show()iii
